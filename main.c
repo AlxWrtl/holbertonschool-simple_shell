84fbcf4b8a74d309a6fb94dd068841bfcdb46b6d
SuperPims <indexes-benzine0p@icloud.com>
2023-12-17 17:51:31 +0100

    Set up main shell entry point in shell.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "lib.h"
+
+/**
+ * main - Entry point for the shell program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * This function serves as the entry point for the shell program. It checks the
+ * number of command-line arguments to determine whether the program should run
+ * in non-interactive mode or interactive mode. If there are command-line
+ * arguments, it assumes non-interactive mode and calls
+ * the non_interactive_mode function with the provided arguments.
+ * Otherwise, it runs in interactive mode by calling the
+ * interactive_mode function.
+ *
+ * Return: 0 on successful execution.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		non_interactive_mode(argc, argv);
+	}
+	else
+	{
+		interactive_mode();
+	}
+
+	return (0);
+}
