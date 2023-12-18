1f7c788415517cb77e43411599e4b1f1dfa24610
SuperPims <indexes-benzine0p@icloud.com>
2023-12-18 19:37:45 +0100

    change void by int to declare the return to an integer

--- lib.h
+++ lib.h
@@ -12,7 +12,7 @@ char **tokenize_line(const char *line, const char *delim);
 void execmd(char **argv);
 void interactive_mode(void);
 void free_tokens(char **tokens);
-void handle_input(char *input);
+int handle_input(char *input);
 void non_interactive_mode(void);
 
 #endif
