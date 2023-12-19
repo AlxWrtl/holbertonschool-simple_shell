1a98e8adc63e240f701da36343571c967ac506fe
SuperPims <indexes-benzine0p@icloud.com>
2023-12-19 14:03:37 +0100

    change the return by EXIT_SUCCESS

--- interactive.c
+++ interactive.c
@@ -43,7 +43,7 @@ int handle_input(char *input)
 		if (tokens[0] && strcmp(tokens[0], "exit") == 0)
 		{
 			free_tokens(tokens);
-			return (-1);
+			exit (EXIT_SUCCESS);
 		}
 		execmd(tokens);
 		free_tokens(tokens);
