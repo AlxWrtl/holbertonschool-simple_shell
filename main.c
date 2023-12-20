7f7f94512b352ae31aceb0321b80b719776a240a
SuperPims <indexes-benzine0p@icloud.com>
2023-12-20 15:51:06 +0100

    change the betty comment of the code

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include "lib.h"
 
 /**
- * main - Entry point for the shell program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings representing the command-line arguments.
+ * main - Entry point of the program.
  *
- * This function serves as the entry point for the shell program. It checks the
- * number of command-line arguments to determine whether the program should run
- * in non-interactive mode or interactive mode. If there are command-line
- * arguments, it assumes non-interactive mode and calls
- * the non_interactive_mode function with the provided arguments.
- * Otherwise, it runs in interactive mode by calling the
- * interactive_mode function.
+ * Calls 'interactive_mode' function to handle interactive shell operations.
  *
- * Return: 0 on successful execution.
+ * In braces (potentially for conditional or future use), calls 
+ * 'non_interactive_mode' function to handle non-interactive shell operations.
+ *
+ * Returns 0 to indicate successful execution.
  */
 
 int main(void)
